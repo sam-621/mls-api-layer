730d2d50e1d62dcb05b77ca43e98fb4a9aa93a87
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-01-21 13:16:13 -0700

    feat(tasks):  add cron job

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ApiModule } from './api/api.module';
 import { ConfigModule } from '@/lib/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TaskModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [ConfigModule, ApiModule],
+  imports: [ConfigModule, ApiModule, ScheduleModule.forRoot(), TaskModule],
 })
 export class AppModule {}
---
+++ src/app/tasks/tasks.service.ts
@@ -0,0 +1,60 @@
+import { HttpService } from '@nestjs/axios';
+import { writeFile } from 'fs/promises';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { firstValueFrom } from 'rxjs';
+import { MlsAPIResponse, Value } from '../mls/mls.type';
+import * as path from 'path';
+
+@Injectable()
+export class TasksService {
+  private readonly logger = new Logger(TasksService.name);
+  constructor(private readonly httpService: HttpService) {}
+
+  @Cron('45 * * * * *')
+  // @Interval(20000)
+  async handleCron() {
+    try {
+      console.log('\n');
+
+      console.time('properties-cron-job');
+      this.logger.log('Fetching properties corn job started');
+
+      await this.getProperties();
+
+      this.logger.log('Fetching properties corn job Ended successfully');
+      console.timeEnd('properties-cron-job');
+    } catch (error) {
+      this.logger.log('Fetching properties corn job Ended with error');
+      this.logger.error(error);
+      console.timeEnd('properties-cron-job');
+    }
+  }
+
+  private async getProperties() {
+    let data: Value[] = [];
+    let currentNextLink =
+      'https://api-demo.mlsgrid.com/v2/Property?$filter=OriginatingSystemName%20eq%20%27mfrmls%27%20and%20MlgCanView%20eq%20true&$expand=Media,Rooms,UnitTypes';
+
+    while (currentNextLink !== undefined) {
+      const response = await firstValueFrom(
+        this.httpService.get<MlsAPIResponse>(currentNextLink, {
+          headers: {
+            Authorization: 'Bearer d2798b340ac104796196ec227904fb34e09b54af',
+          },
+        }),
+      );
+
+      data = [...data, ...response.data.value];
+      currentNextLink = response.data['@odata.nextLink'];
+    }
+
+    await writeFile(
+      path.join(process.cwd(), 'src/app', 'data.json'),
+      JSON.stringify({ value: data }),
+      'utf8',
+    );
+
+    this.logger.log(`Done! ${data.length} properties saved.`);
+  }
+}
