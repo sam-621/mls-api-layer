759b90c11ee01a7760cfa75f8c4db5cadb4a94db
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-03-18 17:58:25 -0700

    update defaults

---
+++ prisma/migrations/20240319005658_defaults/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Property" ALTER COLUMN "area_name" SET DEFAULT '',
+ALTER COLUMN "county" SET DEFAULT '',
+ALTER COLUMN "status" SET DEFAULT '',
+ALTER COLUMN "description" SET DEFAULT '',
+ALTER COLUMN "city" SET DEFAULT '',
+ALTER COLUMN "pc" SET DEFAULT '',
+ALTER COLUMN "address" SET DEFAULT '';
