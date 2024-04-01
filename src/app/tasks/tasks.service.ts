073c009af52613f855dd05f4f4103e8dbbe3deb0
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-04-01 13:09:50 -0700

    feat(cron):  update corn

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -17,7 +17,7 @@ export class TasksService {
 
   // Run every Friday at 12am
   // @Cron('0 0 * * 5')
-  @Cron('59 17 * * *')
+  @Cron('10 13 * * *')
   async handleCron() {
     try {
       console.log('\n');
