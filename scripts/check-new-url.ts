16d16d3a39c8276791e6ab999a58ee7f8da0bb43
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2024-06-25 20:52:09 -0700

    feat(properties):  fix cron job

--- scripts/check-new-url.ts
+++ scripts/check-new-url.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 export const main = async () => {
   const prisma = new PrismaClient();
@@ -6,13 +6,15 @@ export const main = async () => {
   // const count = await prisma.media.count({
   //   where: { url: { contains: 'd1w4u23c1d7gv3.cloudfront.net' } },
   // });
+  const ids = ['MFR715275188', 'MFR722392243', 'MFR715326576', 'MFR726055642'];
 
-  const lastProperties = await prisma.property.findUnique({
-    where: { mlsId: 'MFR715566746' },
-  });
-
-  console.log({
-    lastProperties,
+  ids.forEach(async (id) => {
+    const lastProperties = await prisma.property.findUnique({
+      where: { mlsId: id },
+    });
+    console.log({
+      lastProperties: lastProperties.id,
+    });
   });
 };
 
